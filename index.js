eb0b3646d7785d0bb163297c168f00fc3ce9ee41
IlyaDreamix <ilyabelij@gmail.com>
2022-06-17 21:29:12 +0300

    1.4.1

---
+++ index.js
@@ -0,0 +1,11 @@
+import { generateSig, generateDeviceId, decodeSid } from "./lib/util/utils.js";
+import { Request } from "./lib/util/request.js";
+import { Client } from "./lib/client.js";
+import { Socket } from "./lib/socket.js";
+
+export {
+    generateSig, generateDeviceId, decodeSid,
+    Request,
+    Client,
+    Socket
+}
